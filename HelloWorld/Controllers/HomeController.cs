56939f6dd9ef9083af58d8349df193f11a6d3922
sabet <sabet@cstructor.com>
2022-04-14 21:21:20 -0700

    Session 1 Lecture

---
+++ HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using HelloWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            return View("Thanks", guestResponse);
+        }
+
+    }
+}
---
+++ HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloWorld.Models
+{
+    public class GuestResponse
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public bool? WillAttend { get; set; }
+    }
+}
